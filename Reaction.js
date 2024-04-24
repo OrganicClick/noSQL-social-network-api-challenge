bf0cc885ee4258f4bd36a67dfff523fd5fc3afce
Austin Luper <austin.luper@gmail.com>
2024-04-24 13:59:55 -0400

    Defined the reaction schema and logic to export the reactionSchema in the Reaction.js file

--- Reaction.js
+++ Reaction.js
@@ -4,3 +4,27 @@ const { Schema } = require('mongoose');
 
 // Define the reaction schema
 
+const reactionSchema = new Schema({
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+
+    }
+  });
+  
+  // Export reactionSchema
+  
+  module.exports = reactionSchema;
