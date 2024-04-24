f275984614e407a9b86531b2f98e2addfbf94244
Austin Luper <austin.luper@gmail.com>
2024-04-24 14:15:05 -0400

    Replaced the pseudocode in the Thought.js file with functional logic within the thoughtSchema

--- Thought.js
+++ Thought.js
@@ -7,22 +7,23 @@ const thoughtSchema = new Schema(
     {
       thoughtText: {
         type: String,
-        //required: true,
-        // Must be between 1 and 280 characters,
+        required: true,
+        minLength: 1, // Minimum length of 1 character
+        maxLength: 280, // Maximum length of 280 characters
       },
       createdAt: {
-        Date: //Date logic,
-        // Set default value to the current timestamp
+        type: Date,
+        default: Date.now, // Set default value to the current timestamp
         // Use a getter method to format the timestamp on query
       },
-      username: // The username of the user who created this thought
-      // Use Mongoose's ObjectId data type
+      username: {// The username of the user who created this thought
       // Default value is the current user
         type: String,
-        required: true,
-      reactions: [reactionSchema],
+        required: true, // The username of the user who created this thought
+      },
+      reactions: [reactionSchema], // Using Reaction schema as a subdocument schema for reactions
     },
-  )
+  );
 
 // Define virtual property to calculate the length of the thought's 'reactions' array field on query
 thoughtSchema.virtual('reactionCount').get(function() {
