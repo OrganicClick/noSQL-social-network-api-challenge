b928a9327053290d279ddc7562a654fd1f881b76
Austin Luper <austin.luper@gmail.com>
2024-04-24 15:59:50 -0400

    Added index.js file to the api subfolder in the application

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thoughtRoutes'); // Import thoughtRoutes file
-const userRoutes = require('./userRoutes'); // Import userRoutes file
+const thoughtRoutes = require('../thoughtRoutes'); // Import thoughtRoutes file
+const userRoutes = require('../userRoutes'); // Import userRoutes file
 
 router.use('/thoughts', thoughtRoutes); // Define routes for thoughts under /thoughts URL
 router.use('/users', userRoutes); // Define routes for users under /users URL
