81680425b22e94fa4fb9e1f62cde08a687e3cc28
Austin Luper <austin.luper@gmail.com>
2024-04-24 15:44:01 -0400

    Populated thoughtRoutes.js file with placeholder commented out logic for routing thoughts in the application. Also imported router from Express package and export the router.

---
+++ thoughtRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+//Placeholder text for importing userController functions
+// const {} = require('../../controllers/thoughtController');
+
+// // /api/thought
+// router.route('/').get(getThought).post(createThought);
+
+// // /api/users/:userId
+// router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+module.exports = router;
