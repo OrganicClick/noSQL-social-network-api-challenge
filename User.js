fd488c9ca3c728f6ae1b33a8c16f901823928558
Austin Luper <austin.luper@gmail.com>
2024-04-24 10:16:37 -0400

    Created logic to define userSchema within User.js file

--- User.js
+++ User.js
@@ -1,1 +1,23 @@
 const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      // unique: true,
+      //required: true,
+      // trimmed: true,
+    },
+    email: {
+      type: String,
+      // required: true,
+      // unique: true,
+      // match: must match a valid email address,
+      // validate: [validateEmail, 'Please fill a valid email address'], (??)
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+)
