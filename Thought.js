f6b598e82d5a297e439eb132a2c92dc6e86d7fea
Austin Luper <austin.luper@gmail.com>
2024-04-24 10:49:11 -0400

    Created initial logic with pseudocode placeholders into the Thought.js file defining the model structure

--- Thought.js
+++ Thought.js
@@ -1,1 +1,26 @@
 const { Schema, model } = require('mongoose');
+const { reactionSchema } = require('./Reaction');
+
+// Schema to create Thought model
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        //required: true,
+        // Must be between 1 and 280 characters,
+      },
+      createdAt: {
+        Date: //Date logic,
+        // Set default value to the current timestamp
+        // Use a getter method to format the timestamp on query
+      },
+      username: // The username of the user who created this thought
+      // Use Mongoose's ObjectId data type
+      // Default value is the current user
+        type: String,
+        required: true,
+      reactions: [reactionSchema],
+    },
+  )
+  
