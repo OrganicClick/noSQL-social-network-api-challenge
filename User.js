a0bac5075998ed7b7c15fe3bf0db93111e5c435d
Austin Luper <austin.luper@gmail.com>
2024-04-24 14:27:10 -0400

    Adjusted whitespace and spacing in the User.js file. Also added logic modifying the JSON information sent with userSchema data, enabling virtuals, and excluding the default _id field

--- User.js
+++ User.js
@@ -1,26 +1,30 @@
 const { Schema, model } = require('mongoose');
 
 // Schema to create User model
-
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      unique: true,
       required: true,
+      unique: true,
       trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, // Regular expression to validate email format,
-      // validate: [validateEmail, 'Please fill a valid email address'], (??)
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, // Regular expression to validate email format
     },
     thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
     friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   },
-)
+  {
+    toJSON: {
+      virtuals: true, // Include virtual properties when converting document to JSON
+    },
+    id: false, // Exclude the default '_id' field
+  }
+);
 
 // Define virtual property to calculate friend count
 userSchema.virtual('friendCount').get(function() {
