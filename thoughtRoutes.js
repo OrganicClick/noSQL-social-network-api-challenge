14bdcd7aa9edf43b26af6e6ed096c4ccd41623f6
Austin Luper <austin.luper@gmail.com>
2024-04-24 15:45:47 -0400

    Corrected thoughtRoutes.js file with accurate route pathing

--- thoughtRoutes.js
+++ thoughtRoutes.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 //Placeholder text for importing userController functions
 // const {} = require('../../controllers/thoughtController');
 
-// // /api/thought
+// // /api/thoughts
 // router.route('/').get(getThought).post(createThought);
 
-// // /api/users/:userId
+// // /api/thoughts/:thoughtId
 // router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 module.exports = router;
