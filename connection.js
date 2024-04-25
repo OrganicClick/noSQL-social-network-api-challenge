36c7834d6fb5c4a17d92357268a17a6bc5c7d70c
Austin Luper <austin.luper@gmail.com>
2024-04-25 18:11:42 -0400

    Created a connection.js file file with hopefully the right MongoDB URL to connect to the MongoDB database, socialMedia_DB

--- connection.js
+++ connection.js
@@ -1,3 +1,11 @@
-// Import Mongoose
+// Import necessary functions and objects from the mongoose package
 const { connect, connection } = require('mongoose');
 
+// Define the MongoDB connection string
+const connectionString = 'mongodb://localhost:27017/socialMedia_DB';
+
+// Connect to the MongoDB database using the connection string
+connect(connectionString);
+
+// Export the connection object, representing the MongoDB connection
+module.exports = connection;
