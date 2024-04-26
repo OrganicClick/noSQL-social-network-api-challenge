198478c5bef7ea16922600c159572a289a8a9e66
Austin Luper <austin.luper@gmail.com>
2024-04-26 15:49:27 -0400

    Imported mongoose and necessary dependencies into the thoughtController.js and userController.js files

---
+++ thoughtController.js
@@ -0,0 +1,3 @@
+// Import Mongoose and necessary dependencies
+const mongoose = require('mongoose');
+const { Thought } = require('../models');
---
+++ userController.js
@@ -0,0 +1,3 @@
+// Import Mongoose and necessary dependencies
+const mongoose = require('mongoose');
+const { User } = require('../models');
