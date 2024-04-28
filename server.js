e7530e84069afbbb72954d2e27db1c88c0dc6226
Austin Luper <austin.luper@gmail.com>
2024-04-28 15:16:34 -0400

    Wrote logic into server.js file establishing a connection to MongoDB

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const app = express();
 // MongoDB connection string
 const connectionString = 'mongodb://localhost:27017/socialMedia_DB';
 
+// Connect to MongoDB
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
