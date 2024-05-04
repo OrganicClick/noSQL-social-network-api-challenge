5c3026e0cd3b97992286a3ec91e2fee27b6ebd0f
Austin Luper <austin.luper@gmail.com>
2024-05-04 12:57:39 -0400

    Added necessary routing logic to the index.js file in routes api folder to accommodate newly created reactionRoutes.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes'); // Import thoughtRoutes file
 const userRoutes = require('./userRoutes'); // Import userRoutes file
+const reactionRoutes = require('./reactionRoutes'); // Import reactionRoutes file
 
 router.use('/thoughts', thoughtRoutes); // Define routes for thoughts under /thoughts URL
 router.use('/users', userRoutes); // Define routes for users under /users URL
+router.use('/reactions', reactionRoutes); // Define routes for reactions under /reactions URL
 
 module.exports = router; // Export the router
